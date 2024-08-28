21118449851788c24689f28a40c9a0635e29d716
LukaAlderson <lukalderson88@gmail.com>
2024-08-28 16:41:59 +0400

    Add files via upload

--- newton/regist/views.py
+++ newton/regist/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from datetime import date, time, timezone, datetime
+
+
 def registration(request):
-    return render(request, 'regist/reg.html')
+    weekdays = {
+        1: "Понедельник",
+        2: "Вторник",
+        3: "Среда",
+        4: "Четверг",
+        5: "Пятница",
+        6: "Суббота",
+        7: "Воскресенье"
+    }
+    now_time = datetime.now()
+    format_time = f"{weekdays.get(datetime.weekday(now_time))}, {now_time.hour}:{now_time.minute}"
+    return render(request, 'regist/reg.html', context={"now_time": format_time})
